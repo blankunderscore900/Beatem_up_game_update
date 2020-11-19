a53b0616503c2bbe54a357e99a0481b3ec71f913
blankunderscore900 <nicholas_bollong@outlook.com>
2020-11-19 01:07:11 -0400

    Adding buttons
     Adding more buttons to the game meuns

---
+++ Updated_Beatem_Up_Game/Assets/Scripts/Menu/MenuAnimations.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuAnimations : MonoBehaviour
+{
+
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void MenuStart()
+    {
+        Debug.Log("this is working");
+        animator.SetTrigger("NextScreen");
+    }
+    public void SetUpGame()
+    {
+        animator.SetTrigger("BackScreen");
+        animator.SetTrigger("StartingGame");
+    }
+
+    public void LeaveSetUp()
+    {
+
+        animator.SetTrigger("ResetGame");
+        animator.SetTrigger("NextScreen");
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void LeaveMenu()
+    {
+        Debug.Log("This is leaveing the menu");
+        animator.SetTrigger("BackScreen");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("The Game Has quit");
+        Application.Quit();
+    }
+}
