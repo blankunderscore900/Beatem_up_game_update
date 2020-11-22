b74d5b7b8d7ead8420c90026ff21cf467b136f28
blankunderscore900 <nicholas_bollong@outlook.com>
2020-11-22 01:06:06 -0400

    Setting up scripts
    setting up scripts for the game u.i.

---
+++ Updated_Beatem_Up_Game/Assets/Scripts/Game/GM.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class GM : MonoBehaviour
+{
+    // setting up and sorting the player's objects
+    [Header("Player Status")]
+    public float moveSpeed;
+    public int playerLife;
+    public TextMeshProUGUI gameTime;
+
+    // setting up and sorting the Game menu objects
+    [Header("Game Menus")]
+    public GameObject Menu;
+    public GameObject Pause;
+    public static bool gameIsPaused;
+
+    // instance
+    public static GM instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            gameIsPaused = !gameIsPaused;
+            PauseGame();
+        }
+    }
+
+    void PauseGame()
+    {
+        // Pause the game to bring up the pause menu
+        if (gameIsPaused)
+        {
+            Time.timeScale = 0f;
+            Menu.SetActive(false);
+            Pause.SetActive(true);
+        }
+        else
+        {
+            Time.timeScale = 1f;
+            Menu.SetActive(true);
+            Pause.SetActive(false);
+        }
+    }
+}
--- Updated_Beatem_Up_Game/Assets/Scripts/Menu/MenuAnimations.cs
+++ Updated_Beatem_Up_Game/Assets/Scripts/Menu/MenuAnimations.cs
@@ -21,35 +21,43 @@ public class MenuAnimations : MonoBehaviour
 
     public void MenuStart()
     {
+        // Plays the animation for opening to the start menu of the game
         Debug.Log("this is working");
         animator.SetTrigger("NextScreen");
     }
     public void SetUpGame()
     {
+        // Plays the animation for leaving the start menu
+        // of the game and load the start game menu
         animator.SetTrigger("BackScreen");
         animator.SetTrigger("StartingGame");
     }
 
     public void LeaveSetUp()
     {
-
+        // Plays the animation for leaving the start game menu
+        // and load the animation for the start menu
         animator.SetTrigger("ResetGame");
         animator.SetTrigger("NextScreen");
     }
 
     public void StartGame()
     {
+        // loads the game scene 
         SceneManager.LoadScene("Game");
     }
 
     public void LeaveMenu()
     {
+        // Plays the animation for leaving the start menu
         Debug.Log("This is leaveing the menu");
         animator.SetTrigger("BackScreen");
     }
 
     public void GameAbout()
     {
+        // Plays the animation for leaving the options menu
+        // and plays the animation for the about page
         Debug.Log("This is leaveing the menu");
         animator.SetTrigger("AboutStart");
         animator.SetTrigger("LeaveOptions");
@@ -57,6 +65,8 @@ public class MenuAnimations : MonoBehaviour
 
     public void LeaveAbout()
     {
+        // Plays the animation for leaving the about screen
+        // and plays the animations for going back to the options screen
         Debug.Log("This is leaveing the menu");
         animator.SetTrigger("AboutLeave");
         animator.SetTrigger("OptionsMenu");
@@ -64,6 +74,8 @@ public class MenuAnimations : MonoBehaviour
 
     public void OptionsMenu()
     {
+        // Plays the animation for leaving the start menu
+        // and plays the animations for going to the options screen
         Debug.Log("This is leaveing the menu");
         animator.SetTrigger("BackScreen");
         animator.SetTrigger("OptionsMenu");
@@ -71,6 +83,8 @@ public class MenuAnimations : MonoBehaviour
 
     public void LeaveOptions()
     {
+        // Plays the animation for leaving the options menu
+        // and plays the animations for going to the start menu
         Debug.Log("This is leaveing the menu");
         animator.SetTrigger("LeaveOptions");
         animator.SetTrigger("NextScreen");
@@ -78,6 +92,7 @@ public class MenuAnimations : MonoBehaviour
 
     public void QuitGame()
     {
+        // Quits the game 
         Debug.Log("The Game Has quit");
         Application.Quit();
     }
